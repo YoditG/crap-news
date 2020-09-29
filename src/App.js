015193f8bb9dca9f6201d6e8b517de41c4109fbd
Your Name <y.geb@web.de>
2020-09-29 18:44:56 +0200

    fetch data display link articles

--- src/App.js
+++ src/App.js
@@ -1,26 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
 import logo from './logo.svg';
 import './App.css';
+//import FetchData from './components/API.js'
+
 
 function App() {
+  const [fetchData,setFetchData] = useState(null)
+  //const [userInput,setUserInput] =useState("")
+  
+ // + userInput)
+  useEffect(
+    () => {
+      fetch('http://hn.algolia.com/api/v1/search?query=react')
+      .then(res => res.json())
+      .then(data=>setFetchData(data))
+      }
+    ,[]) 
+    fetchData&&console.log(fetchData)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {fetchData&&fetchData.hits.map((element,index)=>{
+        return (
+        <Fragment key={index}>
+          <div>
+            <h2>
+              <a href={element.url} target="_blank">{element.title}</a>
+            </h2>
+          </div>
+        </Fragment>
+        )
+      })}
     </div>
   );
 }
 
+//  <form>
+//  <input type="text" placeholder="search" onChange={(e)=>setUserInput(e.target.value)}></input>
+  //</form>
+
 export default App;
+
