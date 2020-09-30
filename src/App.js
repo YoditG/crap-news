288928c170e58b89d59802e51d1c1a83eac5914b
Your Name <y.geb@web.de>
2020-09-30 11:00:10 +0200

    finish functionality of search bar

--- src/App.js
+++ src/App.js
@@ -1,43 +1,52 @@
-import React, { useState, useEffect, Fragment } from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect, Fragment, useRef } from 'react';
+//import logo from './logo.svg';
 import './App.css';
-//import FetchData from './components/API.js'
 
 
 function App() {
   const [fetchData,setFetchData] = useState(null)
-  //const [userInput,setUserInput] =useState("")
+  const [userInput,setUserInput] =useState("bla")
   
- // + userInput)
+  const searchBar = useRef(null)
+
   useEffect(
-    () => {
-      fetch('http://hn.algolia.com/api/v1/search?query=react')
+    () => { 
+      searchBar.current.focus()
+      console.log(searchBar.current[0].value)
+      setUserInput(searchBar.current[0].value)
+      const url = `http://hn.algolia.com/api/v1/search?query=${searchBar.current[0].value}`
+      console.log(url)
+      fetch(url)
       .then(res => res.json())
       .then(data=>setFetchData(data))
       }
-    ,[]) 
-    fetchData&&console.log(fetchData)
-
+    ,[userInput]) 
+    
   return (
+    <>
+    <div >
+    
+      <form ref = {searchBar}>
+        <input type="text" placeholder="search" onChange={(e)=>setUserInput(e.target.value)} ></input>
+      </form>
+    </div>
     <div>
       {fetchData&&fetchData.hits.map((element,index)=>{
         return (
         <Fragment key={index}>
           <div>
             <h2>
-              <a href={element.url} target="_blank">{element.title}</a>
+              <a href={element.url} target="_blank" rel="noopener noreferrer">{element.title}</a>
             </h2>
           </div>
         </Fragment>
         )
       })}
     </div>
+    </>
   );
 }
 
-//  <form>
-//  <input type="text" placeholder="search" onChange={(e)=>setUserInput(e.target.value)}></input>
-  //</form>
 
 export default App;
 
