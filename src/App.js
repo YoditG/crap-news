c7f78703971485683d13c5455b7fe6fd22994997
Your Name <y.geb@web.de>
2020-09-30 23:06:43 +0200

    add spinner

--- src/App.js
+++ src/App.js
@@ -1,37 +1,46 @@
-import React, { useState, useEffect, Fragment, useRef } from 'react';
+import React, { useState, useEffect, Fragment} from 'react';
 //import logo from './logo.svg';
 import './App.css';
+import ClipLoader from "react-spinners/ClipLoader";
 
 
 function App() {
   const [fetchData,setFetchData] = useState(null)
-  const [userInput,setUserInput] =useState("bla")
+  const [userInput,setUserInput] =useState("")
+  const [userQuery,setUserQuery] =useState("")
   
-  const searchBar = useRef(null)
 
   useEffect(
     () => { 
-      searchBar.current.focus()
-      console.log(searchBar.current[0].value)
-      setUserInput(searchBar.current[0].value)
-      const url = `http://hn.algolia.com/api/v1/search?query=${searchBar.current[0].value}`
+      const url = `http://hn.algolia.com/api/v1/search?query=${userQuery}`
       console.log(url)
       fetch(url)
       .then(res => res.json())
       .then(data=>setFetchData(data))
       }
-    ,[userInput]) 
-    
+    ,[userQuery]) 
+
+    //onChange: anonymous function (e)=>setUserInput(e.target.value) is similar to:
+    // input: onChange={handleChange}
+    //const handleChange = (e) => {
+  //    setUserInput(e.target.value);
+  //  }
+
+  //onClick = {(e)=> setUserQuery(userInput)} is similar to
+  //input: onClick={handleClick}
+  //const handleClick = (e) => {
+  //  setUserQuery(userInput);
+//   }
+
+
   return (
     <>
-    <div >
-    
-      <form ref = {searchBar}>
+    <div>
         <input type="text" placeholder="search" onChange={(e)=>setUserInput(e.target.value)} ></input>
-      </form>
+        <button onClick={(e)=> setUserQuery(userInput)}>Search</button>
     </div>
     <div>
-      {fetchData&&fetchData.hits.map((element,index)=>{
+      {fetchData?(fetchData.hits.map((element,index)=>{
         return (
         <Fragment key={index}>
           <div>
@@ -41,7 +50,9 @@ function App() {
           </div>
         </Fragment>
         )
-      })}
+      })):(
+        <ClipLoader/>
+      )}
     </div>
     </>
   );
